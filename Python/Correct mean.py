173586bae9c1e3dc9c511d741ef70c9f3ef8d376
vireetha <dudalaviree69@gmail.com>
2024-10-24 04:59:25 +0530

    5

---
+++ Python/Correct mean.py
@@ -0,0 +1,12 @@
+mean1 = 38
+wrong_number =36
+correct_number = 56
+total_number = 40
+
+sum = mean1*total_number
+print("The sum of 40 numbers:",sum)
+SUM2 =sum-(wrong_number)+(correct_number)
+print("CORRECTED SUM", SUM2)
+
+mean2 = SUM2/total_number
+print(mean2)
